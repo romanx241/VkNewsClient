c3acd67542ff6d50d0ddb6d6697c1ae22099bccd
romanx241 <hohlovr@mail.ru>
2023-04-01 10:44:29 +0300

    lesson-5

--- app/src/main/java/ru/netology/vknewsclient/MainActivity.kt
+++ app/src/main/java/ru/netology/vknewsclient/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import ru.netology.vknewsclient.ui.theme.MainScreen
 import ru.netology.vknewsclient.ui.theme.PostCard
 import ru.netology.vknewsclient.ui.theme.VkNewsClientTheme
 
@@ -21,71 +22,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Test()
+            VkNewsClientTheme {
+                MainScreen()
+            }
         }
     }
 }
 
-@Composable
-private fun Test() {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = "TopAppBar title")
-                },
-                navigationIcon = {
-                    IconButton(onClick = {}) {
-                        Icon(Icons.Filled.Menu, contentDescription = null)
-                    }
-                }
-            )
-        },
-        drawerContent = {
-            Text(text = "Text 1")
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(text = "Text 2")
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(text = "Text 3")
-        },
-        bottomBar = {
-            BottomNavigation {
-                BottomNavigationItem(
-                    selected = true,
-                    onClick = {},
-                    icon = {
-                        Icon(Icons.Filled.Favorite, contentDescription = null)
-                    },
-                    label = {
-                        Text(text = "Favourite")
-                    }
-                )
-                BottomNavigationItem(
-                    selected = true,
-                    onClick = {},
-                    icon = {
-                        Icon(Icons.Outlined.Edit, contentDescription = null)
-                    },
-                    label = {
-                        Text(text = "Edit")
-                    }
-                )
-                BottomNavigationItem(
-                    selected = true,
-                    onClick = {},
-                    icon = {
-                        Icon(Icons.Outlined.Delete, contentDescription = null)
-                    },
-                    label = {
-                        Text(text = "Delete")
-                    }
-                )
-            }
-        }
-    ) {
-        Text(
-            modifier = Modifier.padding(it),
-            text = "This is scaffold content"
-        )
-    }
-}
---
+++ app/src/main/java/ru/netology/vknewsclient/ui/theme/NavigationItem.kt
@@ -0,0 +1,31 @@
+package ru.netology.vknewsclient.ui.theme
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.ui.graphics.vector.ImageVector
+import ru.netology.vknewsclient.R
+
+sealed class NavigationItem (
+
+    val titleResId: Int,
+    val icon: ImageVector
+
+    ){
+
+    object Home: NavigationItem(
+        titleResId = R.string.navigation_item_home,
+        icon = Icons.Outlined.Home
+    )
+
+    object Favourite: NavigationItem(
+        titleResId = R.string.navigation_item_favorite,
+        icon = Icons.Outlined.Favorite
+    )
+
+    object Profile: NavigationItem(
+        titleResId = R.string.navigation_item_profile,
+        icon = Icons.Outlined.Person
+    )
+}
